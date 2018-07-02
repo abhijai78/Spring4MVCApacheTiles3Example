e2d614aba74a730d974981742b51f5ddd4d39179
abhijai78 <abhijai78@gmail.com>
2018-07-02 15:13:08 -0500

    Update SpitterController.java

--- src/main/java/com/websystique/springmvc/controller/SpitterController.java
+++ src/main/java/com/websystique/springmvc/controller/SpitterController.java
@@ -11,9 +11,19 @@ public class SpitterController {
 	public String loadRegistrationForm() {
 		return "loadRegisterForm";
 	}
+	
+	@RequestMapping(value = ")
+	public String loadRegistrationForm2() {
+		return "loadRegisterForm1";
+	}
 
 	@RequestMapping(value = "/register")
 	public String showRegistrationForm() {
+		return "registerForm";
+	}
+			
+			@RequestMapping(value = "/register")
+	public String showRegistrationForm4() {
 		return "registerForm";
 	}
 
